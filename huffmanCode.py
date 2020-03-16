9c56541f4bfd1f8ca3d9c5484bdb9717c996abfc
dina alaa <dinaalaaahmed@gmail.com>
2020-03-16 18:49:09 +0200

    adding comments

--- huffmanCode.py
+++ huffmanCode.py
@@ -1,118 +1,151 @@
-f =open("input.txt", "r")
+# read from input file
+f = open("input.txt", "r")
 
 if f.mode == 'r':
     contents = f.read()
 
 print(contents)
+
+# declaring some variables
 char = {}
+encoding = {}
+decoding = {}
+decodingString = ""
+node = []
 
+# declaring frequency dictionary
 for x in contents:
-        if x in char:
-            char[x]+=1
+    if x in char:
+        char[x] += 1
 
-        else:
-            char[x]=1
+    else:
+        char[x] = 1
 
 for x in char:
     print(char[x])
 
+
+# declaring class Node
 class Node:
-    def __init__(self, value, left ,code,right,prop):
+    def __init__(self, value, left, code, right, prop):
         self.value = value
         self.left = left
         self.right = right
         self.code = code
-        self.prop=prop;
-def value(Node):
-    return Node.value
-def addcode(Node,c,char):
-    Node.code+=c;
-    Node.code+=char;
-def code(Node):
-    return Node.code
-def prop(Node):
-    return Node.prop
-def lef(Node):
-    return Node.left
-def righ(Node):
+        self.prop=prop
+
+# declaring some functions
+def value(self):
+    return self.value
+
+
+def add_code(self, c, char):
+    self.code += c
+    self.code += char
+
+
+def code(self):
+    return self.code
+
+
+def prop(self):
+    return self.prop
+
+
+def left_node(self):
+    return self.left
+
+
+def right_node(Node):
     return Node.right
 
-node=[]
+# making array of chars
 for x in char:
-    node1=Node(x,0,"",0,char[x]);
+    node1 = Node(x, 0, "", 0, char[x])
     node.append(node1)
-sorted_with_prop=sorted(node, key= prop,reverse=True)
+
+# sort the array of chars with probability
+sorted_with_prop = sorted(node, key=prop, reverse=True)
 
 for x in sorted_with_prop:
     print(value(x))
-node=sorted_with_prop;
+
+node = sorted_with_prop
+
+# make the huffmanCode tree
 
 while True:
     left = node.pop()
     print(value(left)+"left")
-    if(len(node)==0):
+    if len(node) == 0:
         print(value(left)+"done")
         break
     right = node.pop()
     print(value(right)+"right")
-    node2=Node(value(left)+value(right),left,"",right,prop(left)+prop(right))
+    node2 = Node(value(left)+value(right), left, "", right, prop(left)+prop(right))
     print(value(node2))
     node.append(node2)
     sorted_with_prop = sorted(node, key=prop, reverse=True)
-    node=sorted_with_prop
+    node = sorted_with_prop
 
-
-
+# declare codes to each char in the tree
 
 def traverse(node,c):
-        if (righ(node)== 0 and lef(node)==0 ):
-            return
-        if(lef(node)!=0):
-          addcode(lef(node),c,"0")
-          print(code(lef(node)))
-          traverse(lef(node), code(lef(node)))
-        if (righ(node)!= 0):
-            addcode(righ(node),c,"1")
-            print(code(righ(node)))
-            traverse(righ(node), code(righ(node)))
-encoding = {}
-decoding = {}
-def traverseandmap(node):
-    if (righ(node) == 0 and lef(node) == 0):
-        encoding[value(node)]=code(node)
-        decoding[code(node)]=value(node)
+    if right_node(node) == 0 and left_node(node) == 0:
         return
-    if (lef(node) != 0):
-        traverseandmap(lef(node))
-    if (righ(node) != 0):
-        traverseandmap(righ(node))
+    if left_node(node) != 0:
+        add_code(left_node(node), c, "0")
+        print(code(left_node(node)))
+        traverse(left_node(node), code(left_node(node)))
+    if right_node(node) != 0:
+        add_code(right_node(node), c, "1")
+        print(code(right_node(node)))
+        traverse(right_node(node), code(right_node(node)))
+
+# get codes of each char in tree and put it encoding and decoding dictionary
+def traverse_and_map(node):
+    if right_node(node) == 0 and left_node(node) == 0:
+        encoding[value(node)] = code(node)
+        decoding[code(node)] = value(node)
+        return
+    if left_node(node) != 0:
+        traverse_and_map(left_node(node))
+    if right_node(node) != 0:
+        traverse_and_map(right_node(node))
 
 
-traverse(left,"")
-traverseandmap(left)
+traverse(left, "")
+traverse_and_map(left)
+
 for x in encoding:
     print(encoding[x]+x)
 
-encode=""
+encode = ""
+
+# encode the input file
+
 for x in contents:
-    encode+=encoding[x]
+    encode += encoding[x]
 
 
-f=open("encoding.txt", "+w")
+f = open("encoding.txt", "+w")
 f.write(encode)
 
-f=open("encoding.txt", "+r")
-decode=f.read()
+f = open("encoding.txt", "+r")
+decode = f.read()
 
-f=open("decoding.txt", "+w")
+f = open("decoding.txt", "+w")
 
-decodingstring=""
-y="";
+
+y = ""
+
+# decode the encoded file
+
 for x in decode:
-    y+=x;
-    if(y in decoding):
-        decodingstring+=decoding[y];
-        y=""
+    y += x
+    if y in decoding:
+        decodingString += decoding[y]
+        y = ""
 
 
-f.write(decodingstring)
+f.write(decodingString)
